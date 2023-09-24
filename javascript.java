0b5e26fe02a29f50e9a1262269c2e3edee00251c
Taiara-Oliveira <137340630+Taiara-Oliveira@users.noreply.github.com>
2023-09-24 18:30:28 -0300

    Add files via upload

---
+++ javascript.java
@@ -0,0 +1,6 @@
+ if (typeof goper != 'undefined' && oper == 'resultado') {
+      gvalor = calcOper(goper, gvalor, valor);
+      document.calcform.visor.value = gvalor;
+      delete oper;
+      delete gvalor;
+      return(0);}
